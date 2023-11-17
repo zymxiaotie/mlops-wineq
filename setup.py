5ea485648c5c31aae7b5093c77fbda73b0401fde
May <zymxiaotie@gmail.com>
2023-11-17 16:34:50 +0800

    setup.py file created and installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages # packages in src
+
+setup(
+    name="src",
+    version="0.0.0",
+    description="it is a wine Q package",
+    author="zymxiaotie",
+    packages=find_packages(),
+    license="MIT"
+)
