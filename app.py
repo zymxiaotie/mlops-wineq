1ffac37f89fb8534abbfe0c3d2906d8078d3067f
May <zymxiaotie@gmail.com>
2023-11-18 22:48:07 +0800

    webapp created

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import yaml
 import joblib
 import numpy as np
+# from src import get_data 
 
 params_path = "params.yaml"
 webapp_root = "webapp"
@@ -14,12 +15,49 @@ app = Flask(__name__,
             static_folder=static_dir, 
             template_folder=template_dir)
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def predict(data):
+    config=read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data)
+    print(prediction)
+    return prediction
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response= predict(data)
+        response= {"response":response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error": "Something went wrong!! Try again later!"}
+        return error
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        pass
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                data = np.array([list(map(float, data))])
+                response = predict(data)
+                return render_template("index.html", response=response)
+            elif request.json:
+                response = api_response(request)
+                return jsonify(response)
+
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again later!"}
+            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
+    
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
