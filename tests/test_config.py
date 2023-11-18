12c6b71381b4bb83c880c9a41a06ae86d237baa2
May <zymxiaotie@gmail.com>
2023-11-18 15:27:46 +0800

    pytest example NotInRAnge

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,19 @@
+import pytest 
 
-
+class NotInRange(Exception):
+    def __init__(self, message="value not in range"):
+        self.message = message
+        super().__init__(self.message)
 
 # example of a test case
-def test_generic():
-    a = 2
-    b = 2
-    assert a==b
+def test_generic(): # convention: start with "test_..."
+    a = 5
+    with pytest.raises(NotInRange):
+        if a not in range(10, 20):
+            raise NotInRange
+    
+# # example of a test case
+# def test_generic():
+#     a = 2
+#     b = 2
+#     assert a==b
